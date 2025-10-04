74614d66e0ee473d2bcd009aba2f4245cdb3294e
Whaeedil <baoppyhossen1234@gmail.com>
2025-10-04 22:48:44 +0600

    specialties get and delete

--- src/app/modules/specialties/specialties.controller.ts
+++ src/app/modules/specialties/specialties.controller.ts
@@ -5,6 +5,16 @@ import { sendResponse } from "../../../shared/sendResponse";
 import httpStatus from "http-status-codes";
 import { SpecialtiesServices } from "./specialties.service";
 
+const getAllSpecialties = catchAsync(async (req: Request, res: Response) => {
+  const result = await SpecialtiesServices.getAllSpecialties();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Specialties data fetched",
+    data: result,
+  });
+});
+
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   const result = await SpecialtiesServices.insertIntoDB(req);
 
@@ -16,7 +26,21 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
+const deleteFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const result = await SpecialtiesServices.deleteFromDB(id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Specialty deleted successfully",
+    data: result,
+  });
+});
 
 export const SpecialtiesController = {
   insertIntoDB,
+  getAllSpecialties,
+  deleteFromDB,
 };
--- src/app/modules/specialties/specialties.service.ts
+++ src/app/modules/specialties/specialties.service.ts
@@ -3,6 +3,11 @@ import { fileUploader } from "../../../helpers/fileUploader";
 import prisma from "../../../shared/prisma";
 import { IFile } from "../../interfaces/file";
 
+const getAllSpecialties = async () => {
+  const result = await prisma.specialties.findMany();
+  return result;
+};
+
 const insertIntoDB = async (req: Request) => {
   const file = req.file as IFile;
   if (file) {
@@ -18,6 +23,16 @@ const insertIntoDB = async (req: Request) => {
   return result;
 };
 
+const deleteFromDB = async (id: string) => {
+  const result = await prisma.specialties.delete({
+    where: { id },
+  });
+
+  return result;
+};
+
 export const SpecialtiesServices = {
   insertIntoDB,
+  getAllSpecialties,
+  deleteFromDB,
 };
