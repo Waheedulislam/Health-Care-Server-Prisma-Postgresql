183b3a3c547e1933a71f56243a9b4a375cf3b4f1
Whaeedil <baoppyhossen1234@gmail.com>
2025-10-14 22:27:49 +0600

    docotr-secdulle

--- src/app/modules/DoctorSchedule/doctorSchedule.controller.ts
+++ src/app/modules/DoctorSchedule/doctorSchedule.controller.ts
@@ -3,19 +3,22 @@ import { sendResponse } from "../../../shared/sendResponse";
 import httpStatus from "http-status-codes";
 import { catchAsync } from "../../../shared/catchAsync";
 import { DoctorScheduleServices } from "./doctorSchedule.service";
+import { IAuthUser } from "../../interfaces/common";
 
-const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
-  const user = req.user;
+const insertIntoDB = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const user = req.user;
 
-  const result = await DoctorScheduleServices.insertIntoDB(user, req.body);
+    const result = await DoctorScheduleServices.insertIntoDB(user, req.body);
 
-  sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: "Doctor Schedule Insert successfully",
-    data: result,
-  });
-});
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Doctor Schedule Insert successfully",
+      data: result,
+    });
+  }
+);
 
 export const DoctorScheduleController = {
   insertIntoDB,
